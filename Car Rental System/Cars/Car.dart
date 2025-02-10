17347abc4bc4a06344e6da81ccb9aea5a69c76d7
Abdelaziz Ali <122082712+abdelaziz2642004@users.noreply.github.com>
2025-02-10 09:02:07 +0200

    Update Car.dart

--- Car Rental System/Cars/Car.dart
+++ Car Rental System/Cars/Car.dart
@@ -1,32 +1,35 @@
 import 'package:uuid/uuid.dart';
+// made the variables private and added getters 
 
 abstract class Car {
   String _id;
   int _year; // model ?
-  double rentPriceAday;
-  bool available;
+  double _rentPriceAday;
+  bool _available;
 
-  Car(this._year, this.rentPriceAday, this.available) : _id = Uuid().v4();
+  Car(this._year, this._rentPriceAday, this._available) : _id = Uuid().v4();
 
   set setAvailable(bool available) {
-    this.available = available;
+    _available = available;
   }
 
   set setRentPriceAday(double price) {
-    this.rentPriceAday = price;
+    _rentPriceAday = price;
   }
 
   void displayCarDetails() {
     print("Car ID: $_id");
     print("Year: $_year");
-    print("Rent Price: $rentPriceAday");
-    print("Available: $available");
+    print("Rent Price: $_rentPriceAday");
+    print("Available: $_available");
   }
 
   double calcCost(double days) {
-    return days * rentPriceAday;
+    return days * _rentPriceAday;
   }
 
   String get id => _id;
   int get year => _year;
+  double get rentPriceAday => _rentPriceAday;
+  bool get available => _available;
 }
